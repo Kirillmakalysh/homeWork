454f2e8ebdeaee8bbf421715f61484aa1d19e671
Kirill <makaysh.kirill@mail.ru>
2023-06-25 00:41:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.Write("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine("Квадрат числа " + num + " равен: " + result);
+
+
+// Console.Write("Введите первое число ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+// if (firstNum == secondNum * secondNum)
+// {
+//     Console.WriteLine("Число " + firstNum + " является Квадратом числа " + secondNum);
+// }
+// else
+// {
+//     Console.WriteLine("Число " + firstNum + " Не является Квадратом числа " + secondNum);
+// }
+
+
+// Console.Write("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num < 0 )
+// {
+//     num = num * (-1);
+// }
+// int negative = num * (-1);
+// while(negative <= num)
+// {
+//     Console.Write(negative + " ");
+//     negative++;
+// }
+// Console.WriteLine();
+
+
